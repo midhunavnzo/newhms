b7c4c22741434daef39e7f81eed87ebcaffaa98a
AdharshPS <adharshps000@gmail.com>
2024-12-04 10:45:30 +0530

    deleted

--- hmsprjct/hmsapp/views.py
+++ hmsprjct/hmsapp/views.py
@@ -398,19 +398,3 @@ class FileUpload(APIView):
             "message": "File uploaded successfully.",
             "file_path": f"reports/{file.name}"
         }, status=201)
-    
-# class DepartmentListView(generics.ListAPIView):
-#     def get_queryset(self):
-#         queryset = Department.objects.all()
-#         name = self.request.query_params.get('name', None)
-#         if name:
-#             queryset = queryset.filter(name__icontains=name)
-#         return queryset
-
-#     def list(self, request, *args, **kwargs):
-#         queryset = self.get_queryset()
-#         # Extract the department names as a list
-#         department_names = list(queryset.values_list('name', flat=True))
-#         # Return only the list of names
-#         return Response(department_names)
-
